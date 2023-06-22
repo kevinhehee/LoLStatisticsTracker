daf4cdf8b09ebb20892cb2d6b2d7e1528b3e6bc7
Kevin He <kevin.he6333@gmail.com>
2023-06-22 02:19:43 -0700

    formatted left data

--- client/src/App.js
+++ client/src/App.js
@@ -33,26 +33,57 @@ function App() {
         <div className = "leftdataContainer">
           {gameList[0].length !== 0 ? 
           <>
-            <div>
 
-              <div className = "userInfo">
-                <h1>{gameList[0][0]}</h1> 
-                <p>Player Level: {gameList[0][1]}</p>
-                <img width= "100px" height = "100px" src = {"http://ddragon.leagueoflegends.com/cdn/13.11.1/img/profileicon/" + 
-                gameList[0][2] + ".png"}></img>
+
+              <div className = "userInfoContainer">
+                <div className = "summonerName">
+                  <h1>{gameList[0][0]}</h1> 
+                  <img min-width = "50%" min-height = "50%" src = {"http://ddragon.leagueoflegends.com/cdn/13.11.1/img/profileicon/" + 
+                      gameList[0][2] + ".png"}>
+                  </img>
+                  <p>Level {gameList[0][1]}</p>
+                </div>
+
+                <div className = "portraitContainer">
+
+                  <div className = "HighestMastery">
+                    <h1 style = {{textAlign: 'center'}} >Highest Mastery</h1>
+                  </div>
+                  
+                  <div className = "champSplashContainer">
+
+                    <div className = "champSplash">
+                      <h1>{gameList[3][0]}</h1>
+                      <img width = "231px" height = "420px" src = {"http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + gameList[3][0] + "_0.jpg"}></img>
+                      <masteryPoints>{gameList[2][1]} pts</masteryPoints>
+                    </div>
+
+                    <div className = "champSplash">
+                      <h1>{gameList[3][1]}</h1>
+                      <img width = "231px" height = "420px" src = {"http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + gameList[3][1] + "_0.jpg"}></img>
+                      <masteryPoints>{gameList[2][3]} pts</masteryPoints>
+                    </div>
+
+                    <div className = "champSplash">
+                      <h1>{gameList[3][2]}</h1>
+                      <img width = "231px" height = "420px" src = {"http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + gameList[3][2] + "_0.jpg"}></img>
+                      <masteryPoints>{gameList[2][5]} pts</masteryPoints>
+                    </div>
+                    
+                  </div>
+                </div>
+
+
               </div>
 
-              <div className = "Portraits">
-                <h1>Highest Mastery</h1>
-                
-                <champ>{gameList[3][0]}</champ>
-                <img width = "154px" height = "280px" src = {"http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + gameList[3][0] + "_0.jpg"}></img>
-                <champ>{gameList[3][1]}</champ>
-                <img width = "154px" height = "280px" src = {"http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + gameList[3][1] + "_0.jpg"}></img>
-                <champ>{gameList[3][2]}</champ>
-                <img width = "154px" height = "280px" src = {"http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + gameList[3][2] + "_0.jpg"}></img>
-              </div>
-            </div>
+              
+
+              
+
+              {/* <div className = "rankInfoContainer">
+                <h1>Hello </h1>
+
+              </div> */}
             
           </>
           :
@@ -60,9 +91,6 @@ function App() {
           No player
           </>
           }
-          
-
-
         </div>  
 
         
