0a3b123f282ff63ece9a658a81cef547f165d25a
kevinhehee <qweasd602og@gmail.com>
2023-08-13 11:03:18 -0700

    Updated to new Riot API call to get Champion Mastery Points

--- proxyServer.js
+++ proxyServer.js
@@ -26,17 +26,18 @@ function getPlayerDATA(playerName) {
             userInfo.push(response.data.name);
             userInfo.push(response.data.summonerLevel);
             userInfo.push(response.data.profileIconId);
+            userInfo.push(response.data.id  )
             // userInfo.push(response.data.id)
-            console.log(response.data);
+            // console.log(response.data);
             return response.data;
         }).catch(err => err);
 }
 
 function getPlayerCHAMP(PUUID) 
 {
-    return axios.get("https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/" + PUUID + "?api_key=" + API_KEY)
+    return axios.get("https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" + userInfo[3] + "/top?api_key=" + API_KEY)
         .then(response => {
-            // console.log("https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/" + PUUID + "?api_key=" + API_KEY);
+            console.log("https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" + userInfo[3] + "/top?api_key=" + API_KEY);
             userChampIDs.push(response.data[0].championId);
             userChampIDs.push(response.data[0].championPoints);
 
@@ -45,6 +46,7 @@ function getPlayerCHAMP(PUUID)
 
             userChampIDs.push(response.data[2].championId);
             userChampIDs.push(response.data[2].championPoints);
+
         }).catch(err => err);
 }
 
@@ -53,7 +55,9 @@ function getChampfromID(PUUID)
     return axios.get("http://ddragon.leagueoflegends.com/cdn/13.11.1/data/en_US/champion.json")
     .then(response => 
     {
-        let allchamps = ["Aatrox","Ahri","Akali","Akshan","Alistar","Amumu","Anivia","Annie","Aphelios","Ashe","AurelionSol","Azir","Bard","Belveth","Blitzcrank","Brand","Braum","Caitlyn","Camille","Cassiopeia","Chogath","Corki","Darius","Diana","Draven","DrMundo","Ekko","Elise","Evelynn","Ezreal","Fiddlesticks","Fiora","Fizz","Galio","Gangplank","Garen","Gnar","Gragas","Graves","Gwen","Hecarim","Heimerdinger","Illaoi","Irelia","Ivern","Janna","JarvanIV","Jax","Jayce","Jhin","Jinx","Kaisa","Kalista","Karma","Karthus","Kassadin","Katarina","Kayle","Kayn","Kennen","Khazix","Kindred","Kled","KogMaw","Leblanc","LeeSin","Leona","Lillia","Lissandra","Lucian","Lulu","Lux","Malphite","Malzahar","Maokai","MasterYi","MissFortune","Mordekaiser","Morgana","Nami","Nasus","Nautilus","Neeko","Nidalee","Nilah","Nocturne","Nunu","Olaf","Orianna","Ornn","Pantheon","Poppy","Pyke","Qiyana","Quinn","Rakan","Rammus","RekSai","Rell","Renata","Renekton","Rengar","Riven","Rumble","Ryze","Samira","Sejuani","Senna","Seraphine","Sett","Shaco","Shen","Shyvana","Singed","Sion","Sivir","Skarner","Sona","Soraka","Swain","Sylas","Syndra","TahmKench","Taliyah","Talon","Taric","Teemo","Thresh","Tristana","Trundle","Tryndamere","TwistedFate","Twitch","Udyr","Urgot","Varus","Vayne","Veigar","Velkoz","Vex","Vi","Viego","Viktor","Vladimir","Volibear","Warwick","MonkeyKing","Xayah","Xerath","XinZhao","Yasuo","Yone","Yorick","Yuumi","Zac","Zed","Zeri","Ziggs","Zilean","Zoe","Zyra"];
+        let allchamps = ["Aatrox","Ahri","Akali","Akshan","Alistar","Amumu","Anivia","Annie","Aphelios","Ashe","AurelionSol","Azir","Bard","Belveth","Blitzcrank","Brand","Braum","Caitlyn","Camille","Cassiopeia","Chogath","Corki","Darius","Diana","Draven","DrMundo","Ekko","Elise","Evelynn","Ezreal","Fiddlesticks","Fiora","Fizz","Galio","Gangplank","Garen","Gnar","Gragas","Graves","Gwen","Hecarim","Heimerdinger","Illaoi","Irelia","Ivern","Janna","JarvanIV","Jax","Jayce","Jhin","Jinx","Kaisa","Kalista","Karma","Karthus","Kassadin","Katarina","Kayle","Kayn","Kennen","Khazix","Kindred","Kled","KogMaw","Leblanc","LeeSin","Leona","Lillia","Lissandra","Lucian","Lulu","Lux","Malphite","Malzahar","Maokai","MasterYi","MissFortune","Mordekaiser","Morgana","Nami","Naafiri","Nasus","Nautilus","Neeko","Nidalee","Nilah","Nocturne","Nunu","Olaf","Orianna","Ornn","Pantheon","Poppy","Pyke","Qiyana","Quinn","Rakan","Rammus","RekSai","Rell","Renata","Renekton","Rengar","Riven","Rumble","Ryze","Samira","Sejuani","Senna","Seraphine","Sett","Shaco","Shen","Shyvana","Singed","Sion","Sivir","Skarner","Sona","Soraka","Swain","Sylas","Syndra","TahmKench","Taliyah","Talon","Taric","Teemo","Thresh","Tristana","Trundle","Tryndamere","TwistedFate","Twitch","Udyr","Urgot","Varus","Vayne","Veigar","Velkoz","Vex","Vi","Viego","Viktor","Vladimir","Volibear","Warwick","MonkeyKing","Xayah","Xerath","XinZhao","Yasuo","Yone","Yorick","Yuumi","Zac","Zed","Zeri","Ziggs","Zilean","Zoe","Zyra"];
+        
+        
 
         for (let i = 0; i < allchamps.length; i++)
         {
@@ -61,6 +65,7 @@ function getChampfromID(PUUID)
 
             if(check.key === userChampIDs[0].toString())
             {
+                
                 champNames.push(check.id);
                 champNames.push(check.name);
                 break;
@@ -99,8 +104,8 @@ function getRankedInfo(id)
     return axios.get("https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + id + "?api_key=" + API_KEY)
     .then(response =>
         {
-            console.log("https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + id + "?api_key=" + API_KEY);
-            console.log(response.data);
+            // console.log("https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + id + "?api_key=" + API_KEY);
+            // console.log(response.data);
 
             if (response.data.length !== 0)
             {
@@ -121,12 +126,13 @@ app.get('/past5Games', async (req, res) => {
     const playerData = await getPlayerDATA(playerName);
     const PUUID = playerData.puuid;
     const playerChamp = await getPlayerCHAMP(PUUID);
-    console.log(playerData.id);
+    console.log(userChampIDs);
+    // console.log(playerData.id);
     const rankInfo = await getRankedInfo(playerData.id);
 
     const work = await getChampfromID(PUUID);
     const API_CALL = "https://americas.api.riotgames.com" + "/lol/match/v5/matches/by-puuid/" + PUUID + "/ids" + "?api_key=" + API_KEY;
-    console.log(API_CALL);
+    // console.log(API_CALL);
     // API call to find list of game IDs
     const gameIDs = await axios.get(API_CALL)
         .then(response => response.data)
@@ -144,11 +150,12 @@ app.get('/past5Games', async (req, res) => {
         const matchData = await axios.get("https://americas.api.riotgames.com/lol/match/v5/matches/" + matchID + "?api_key=" + API_KEY)
             .then(response => response.data)
             .catch(err => err)
-        console.log("https://americas.api.riotgames.com/lol/match/v5/matches/" + matchID + "?api_key=" + API_KEY);
+        // console.log("https://americas.api.riotgames.com/lol/match/v5/matches/" + matchID + "?api_key=" + API_KEY);
         matchDataArray.push(matchData);
         
-        console.log(matchData);
+        // console.log(matchData);
     }
+    console.log(champNames);
     var allDATA = [userInfo, matchDataArray, userChampIDs, champNames, rankedInfo];
     res.json(allDATA);
     for (let i = 0; i < 6; i++)
